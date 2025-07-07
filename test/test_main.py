8923f6c13cb49737f0153352ba459e2569ea165a
Norihiro Kamae <norihiro@nagater.net>
2025-07-08 02:42:08 +0900

    test: Add test case for ConfigBase error handling

--- test/test_main.py
+++ test/test_main.py
@@ -78,5 +78,25 @@ class TestMainFailure(unittest.TestCase):
             self.assertNotEqual(ret, 0)
             self.assertIn('No such type', mock_error.call_args_list[0][0][0])
 
+    @patch('logging.getLogger')
+    def test_wrong_step_field(self, mock_logger):
+        with tempfile.NamedTemporaryFile(mode='w', delete_on_close=False) as fc:
+            fc.write('''
+            steps:
+              - type: openlp
+                invalid_field: 'error'
+             ''')
+            fc.close()
+
+            mock_logger.return_value = MagicMock()
+            mock_error = mock_logger.return_value.error
+
+            with patch('sys.argv', ['test', '-c', fc.name]):
+                ret = main.main()
+
+            self.assertNotEqual(ret, 0)
+            self.assertIn('Failed to start', mock_error.call_args_list[0][0][0])
+            self.assertIn('is undefined', str(mock_error.call_args_list[0][0][1]))
+
 if __name__ == '__main__':
     unittest.main()
