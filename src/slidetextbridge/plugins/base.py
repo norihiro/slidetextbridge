82eac9685641cb5d5d8f354cfde23be42093a9de
Norihiro Kamae <norihiro@nagater.net>
2025-07-19 21:42:04 +0900

    [WIP] Centralize separated slide classes into SlideBase class

--- src/slidetextbridge/plugins/base.py
+++ src/slidetextbridge/plugins/base.py
@@ -81,19 +81,70 @@ class PluginBase:
         The callback function when updating the new slide.
         '''
 
+def _traverse_obj_for_texts(obj: dict, text_paths):
+    if isinstance(obj, str):
+        yield obj
+        return
+    if isinstance(obj, (int, float, bool)):
+        return
+    if isinstance(obj, dict):
+        if isinstance(text_paths, dict):
+            for key, sub_paths in text_paths.items():
+                if key in obj:
+                    yield from _traverse_obj_for_texts(obj[key], sub_paths)
+        elif isinstance(text_paths, (list, tuple)):
+            for key in text_paths:
+                if key in obj:
+                    yield from _traverse_obj_for_texts(obj[key], text_paths)
+        return
+    if isinstance(obj, (list, tuple)):
+        for item in obj:
+            yield from _traverse_obj_for_texts(item, text_paths)
 
 class SlideBase():
     '''
     The base class representing the current slide
     '''
-    def __init__(self, slide=None, data=None):
-        pass
+
+    _default_text_paths = {
+            'shapes': {
+                'text': None,
+            },
+    }
+
+    def __init__(self, data=None, parent=None):
+        self._dict = data or {}
+        if parent or not hasattr(self, 'parent'):
+            self.parent = parent
+
+    def set_data(self, data):
+        '''
+        Update the data for the slide.
+        :param data: The new data
+        '''
+        self._dict = data
 
     def to_texts(self):
         '''
         List all texts
         :return:  List of strings
+
+        By default, `shapes.[*].text` will be selected.
+        To change the search paths, set `text_paths`.
         '''
+        if not self._dict:
+            return []
+
+        if 'text_paths' in self._dict:
+            text_paths = self._dict['text_paths']
+        else:
+            text_paths = SlideBase._default_text_paths
+
+        try:
+            return list(_traverse_obj_for_texts(self._dict, text_paths))
+        except (TypeError, KeyError) as e:
+            logger = self.parent.logger if self.parent else logging.getLogger('SlideBase')
+            logger.error('Failed to convert slide to texts. %s', e)
         return []
 
     def __str__(self):
@@ -130,4 +181,4 @@ class SlideBase():
         with a `data` parameter to create a new slide object.
         :return: dict object representing this object
         '''
-        return {}
+        return self._dict or {}
