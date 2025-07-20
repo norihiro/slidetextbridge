17bf125328903cbd87da4a80e855b4fc44e77b5d
Norihiro Kamae <norihiro@nagater.net>
2025-07-21 00:57:34 +0900

    test/test_dataset.sh: Reduce log level to warning

--- test/test_dataset.sh
+++ test/test_dataset.sh
@@ -7,6 +7,6 @@ exe=${EXE:-slidetextbridge}
 for cfg in test/data/*.yaml; do
 	set -e
 	echo "Running $cfg..."
-	$exe -c $cfg < ${cfg/.yaml/.in} |
+	$exe -q -c $cfg < ${cfg/.yaml/.in} |
 		diff -u - ${cfg/.yaml/.out}
 done
