2428a1c2babb28da71d25861a982bbe85c1f6bbe
Norihiro Kamae <norihiro@nagater.net>
2025-07-21 09:31:41 +0900

    plugins/accumulate: Exclude exception handlers from code coverage

--- src/slidetextbridge/plugins/accumulate.py
+++ src/slidetextbridge/plugins/accumulate.py
@@ -22,38 +22,38 @@ plugin_classes.append(debug_helper.StdoutEmitter)
 try:
     from . import obsws
     plugin_classes.append(obsws.ObsWsEmitter)
-except ImportError as e:
+except ImportError as e: # pragma: no cover
     logger.info('obsws is unsupported. %s', e)
 
 if _is_win:
     try:
         from . import powerpoint
         plugin_classes.append(powerpoint.PowerPointCapture)
-    except ImportError as e:
+    except ImportError as e: # pragma: no cover
         logger.info('Microsoft PowerPoint is unsupported %s', e)
 
 try:
     from . import impress
     plugin_classes.append(impress.ImpressCapture)
-except ImportError as e:
+except ImportError as e: # pragma: no cover
     logger.info('LibreOffice is unsupported %s', e)
 
 try:
     from . import openlp
     plugin_classes.append(openlp.OpenLPCapture)
-except ImportError as e:
+except ImportError as e: # pragma: no cover
     logger.info('OpenLP is unsupported %s', e)
 
 try:
     from . import jmespath_filter
     plugin_classes.append(jmespath_filter.JMESPathFilter)
-except ImportError as e:
+except ImportError as e: # pragma: no cover
     logger.info('JMESPath filter is unsupported %s', e)
 
 try:
     from . import webserver
     plugin_classes.append(webserver.WebServerEmitter)
-except ImportError as e:
+except ImportError as e: # pragma: no cover
     logger.info('Web server is unsupported %s', e)
 
 plugins = {cls.type_name(): cls for cls in plugin_classes}
