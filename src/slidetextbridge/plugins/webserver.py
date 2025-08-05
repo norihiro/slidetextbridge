norihiro/slidetextbridge

commit 31f37bbb1416e83a061c103227ea46bdacaef180

Fix JavaScript code by eslint

src/slidetextbridge/plugins/webserver.py
@@ -39,11 +39,11 @@ function get_appropriate_ws_url(extra_url) {
 function text_to_html(text) {
     return text.replace(/[&<>"']/g, function (c) {
         return ({
-            '&': '&amp;',
-            '<': '&lt;',
-            '>': '&gt;',
-            '"': '&quot;',
-            "'": '&#39;',
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            "\"": "&quot;",
+            "'": "&#39;",
         })[c];
     }).replace(/\\n/g, "<br/>");
 }
@@ -64,7 +64,7 @@ function connect_ws() {
     ws.onclose = function() {
         ws_failed += 1;
         setTimeout(connect_ws, ws_failed < 6 ? ws_failed * 500 : 3000);
-    }
+    };
 }
 
 document.addEventListener("DOMContentLoaded", function() {

doc/_static/switcher.js
@@ -1,86 +1,86 @@
-'use strict';
+"use strict";
 
-let current_language = DOCUMENTATION_OPTIONS.LANGUAGE?.toLowerCase() || 'en';
+let current_language = DOCUMENTATION_OPTIONS.LANGUAGE?.toLowerCase() || "en";
 let current_root = new URL(document.documentElement.dataset.content_root, window.location).href;
 let current_version = (() => {
-	let aa = current_root.split('/');
+	let aa = current_root.split("/");
 	aa.reverse();
 	for (const a of aa) {
 		if (a.length)
-			return a
+			return a;
 	}
 })();
 let docs_root = (() => {
-	let aa = current_root.split('/');
-	if (aa[aa.length-1] == '')
+	let aa = current_root.split("/");
+	if (aa[aa.length-1] == "")
 		aa.pop();
 	aa.pop();
 	if (aa[aa.length-1] == current_language)
-		aa.pop()
-	return aa.join('/') + '/';
+		aa.pop();
+	return aa.join("/") + "/";
 })();
-let rel_path = window.location.href.replace(current_root, '');
+let rel_path = window.location.href.replace(current_root, "");
 
 async function fetch_switcher_data() {
-	const res = await fetch(docs_root + 'switcher-data.json');
+	const res = await fetch(docs_root + "switcher-data.json");
 	if (!res.ok)
-		throw new Error('Cannot get switcher-data.json')
+		throw new Error("Cannot get switcher-data.json");
 	return await res.json();
 }
 
 async function create_switchers() {
-	if (document.querySelector('div.selectors'))
+	if (document.querySelector("div.selectors"))
 		return;
 
-	const select_lang = document.createElement('select');
-	select_lang.className = 'selector-language';
+	const select_lang = document.createElement("select");
+	select_lang.className = "selector-language";
 
-	const select_ver = document.createElement('select');
-	select_ver.className = 'selector-version';
+	const select_ver = document.createElement("select");
+	select_ver.className = "selector-version";
 
-	const selectors = document.createElement('div');
-	selectors.className = 'selectors';
+	const selectors = document.createElement("div");
+	selectors.className = "selectors";
 	selectors.append(select_lang);
 	selectors.append(select_ver);
 
-	document.querySelector('h1.logo').insertAdjacentElement('afterend', selectors);
+	document.querySelector("h1.logo").insertAdjacentElement("afterend", selectors);
 
 	let data = await fetch_switcher_data();
 
 	for (const lang of data.languages) {
-		const option = document.createElement('option');
-		option.value = lang.language || 'en';
+		const option = document.createElement("option");
+		option.value = lang.language || "en";
 		option.text = lang.display;
 		option.selected = option.value == current_language;
 		select_lang.append(option);
 	}
 
 	for (const v of data.versions) {
-		const option = document.createElement('option');
+		const option = document.createElement("option");
 		option.value = v.version;
 		option.text = v.display;
 		option.selected = option.value == current_version;
 		select_ver.append(option);
 	}
 
-	select_lang.addEventListener('change', on_language_select);
-	select_ver.addEventListener('change', on_version_select);
+	select_lang.addEventListener("change", on_language_select);
+	select_ver.addEventListener("change", on_version_select);
 }
 
 function make_url(language, version) {
 	let new_language = language || current_language;
 	let new_version = version || current_version;
 
-	if (new_language == 'en')
-		return docs_root + new_version + '/' + rel_path;
+	if (new_language == "en")
+		return docs_root + new_version + "/" + rel_path;
 	else
-		return docs_root + new_language + '/' + new_version + '/' + rel_path;
+		return docs_root + new_language + "/" + new_version + "/" + rel_path;
 }
 
 async function navigate_to_first_existing(urls) {
 	for (const url of urls) {
 		try {
-			let res = await fetch(url, {method: 'HEAD'});
+			let res = await fetch(url, {method: "HEAD"});
 			if (res.ok) {
 				window.location.href = url;
 				return url;
@@ -98,7 +98,7 @@ function on_language_select(event) {
 
 	navigate_to_first_existing([
 		make_url(language, current_version),
-		make_url(language, 'latest'),
+		make_url(language, "latest"),
 	]);
 }
 
@@ -109,7 +109,7 @@ function on_version_select(event) {
 
 	navigate_to_first_existing([
 		make_url(current_language, version),
-		make_url('en', version),
+		make_url("en", version),
 	]);
 }
 

commit ffb5753eaf3be49df2c84f854212de84d78b8bfc

webserver: Fix JavaScript syntax error
The error was introduced in 31f37bbb14.

src/slidetextbridge/plugins/webserver.py
@@ -42,7 +42,7 @@ function text_to_html(text) {
             "&": "&amp;",
             "<": "&lt;",
             ">": "&gt;",
-            "\"": "&quot;",
+            "\\"": "&quot;",
             "'": "&#39;",
         })[c];
     }).replace(/\\n/g, "<br/>");